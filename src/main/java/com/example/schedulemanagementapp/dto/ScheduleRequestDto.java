2c021a6d88fd694bb1be9a25d0e9847628677de1
Mini9709 <tjdals9709@naver.com>
2024-11-07 23:23:03 +0900

    feat : @Valid 추가

--- src/main/java/com/example/schedulemanagementapp/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/schedulemanagementapp/dto/ScheduleRequestDto.java
@@ -1,8 +1,9 @@
 package com.example.schedulemanagementapp.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-
-import java.util.Date;
 
 @Getter
 public class ScheduleRequestDto {
@@ -10,7 +11,9 @@ public class ScheduleRequestDto {
     private String title;
     private Long userId;
     private String name;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+    @Max(value = 200, message = "200자 내로 적어주세요.")@NotNull(message = "할 일을 입력해주세요.")
     private String contents;
 
 }
--- src/main/java/com/example/schedulemanagementapp/dto/UserRequestDto.java
+++ src/main/java/com/example/schedulemanagementapp/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.schedulemanagementapp.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 
 @Getter
@@ -7,5 +8,6 @@ public class UserRequestDto {
 
     private Long userId;
     private String name;
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 }
