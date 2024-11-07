955c87bbc4026299a216d0a588aa7379801ebc64
Mini9709 <tjdals9709@naver.com>
2024-11-08 00:20:38 +0900

    feat : 어노테이션 변경

--- src/main/java/com/example/schedulemanagementapp/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/schedulemanagementapp/dto/ScheduleRequestDto.java
@@ -1,8 +1,8 @@
 package com.example.schedulemanagementapp.dto;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -13,7 +13,7 @@ public class ScheduleRequestDto {
     private String name;
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
-    @Max(value = 200, message = "200자 내로 적어주세요.")@NotNull(message = "할 일을 입력해주세요.")
+    @Size(max = 200, message = "200자 내로 적어주세요.")@NotNull(message = "할 일을 입력해주세요.")
     private String contents;
 
 }
